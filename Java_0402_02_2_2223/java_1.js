18fdd13fed9a81a03cef04ee25ff35bde1fc3d3d
Kirillovaad <113346671+Kirillovaad@users.noreply.github.com>
2023-02-27 15:06:56 +0300

    Add files via upload

---
+++ Java_0402_02_2_2223/java_1.js
@@ -0,0 +1,24 @@
+'Привет Java!'
+var myThing;
+myThing=5;
+myThing='это строка';
+var numberNine = 9;
+var stringNine = "9";
+numberNine+numberNine;
+stringNine+stringNine;
+var greeting = "Привет, ";
+var myName = "Ник";
+greeting + myName;
+myName.length
+
+name_orz = " ОРЗ";
+num_orz_pn= 2;
+num_orz_vt = 3;
+num_orz_sr=2;
+name_vetr = " Ветрянка";
+num_vetr_pn= 0;
+num_vetr_vt=1;
+num_vetr_sr =4;
+num_orz_week=num_orz_pn+num_orz_vt+num_orz_sr;
+num_vetr_week=num_vetr_pn + num_vetr_sr+num_vetr_vt;
+num_vetr_week+name_vetr + num_orz_week + name_orz;
